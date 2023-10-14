8ee8489dca0046a5560c935700a28e31c19640f2
Ramyani <ramyani.ghosh@gmail.com>
2023-10-14 12:31:22 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,116 @@
+/*
+ * @name Kaleidoscope
+ * @arialabel User draws thick black lines on the grey background and it is mirrored 5 times in a circle like a kaleidoscope
+ * @description A kaleidoscope is an optical instrument with two or more reflecting surfaces tilted to each other in an angle. This example tries to replicate the behavior of a kaleidoscope. Set the number of reflections at the symmetry variable and start drawing on the screen. Adjust the brush size with the help of the slider. The clear screen as it says clears the screen. The save button will download a .jpg file of the art that you have created.
+ */
+// Symmetry corresponding to the number of reflections. Change the number for different number of reflections 
+let symmetry = 6;  
+
+let angle = 360 / symmetry;
+let saveButton, clearButton, mouseButton, keyboardButton;
+let slider;
+
+function setup() { 
+  createCanvas(710, 710);
+  angleMode(DEGREES);
+  background(28);
+
+  // Creating the save button for the file
+  saveButton = createButton('save');
+  saveButton.mousePressed(saveFile);
+
+  // Creating the clear screen button
+  clearButton = createButton('clear');
+  clearButton.mousePressed(clearScreen);
+
+  // Creating the button for Full Screen
+  fullscreenButton = createButton('Full Screen');
+  fullscreenButton.mousePressed(screenFull);
+
+  // Setting up the slider for the thickness of the brush
+  brushSizeSlider = createButton('Brush Size Slider');
+  sizeSlider = createSlider(1, 100, 4, 0.1);
+    
+  // Create an Audio input
+  mic = new p5.AudioIn();
+
+  // start the Audio Input.
+  // By default, it does not .connect() (to the computer speakers)
+  mic.start();
+}
+
+// Save File Function
+function saveFile() {
+  save('design.jpg');
+}
+
+// Clear Screen function
+function clearScreen() {
+  background(2);
+}
+
+// Full Screen Function
+function screenFull() {
+  let fs = fullscreen();
+  fullscreen(!fs);
+}
+
+function draw() {
+  
+  
+  let vol = mic.getLevel();
+  let freq = getFrequency(vol);
+  console.log(vol);
+  console.log(freq);
+  
+  translate(width / 2, height / 2);
+
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    let mx = mouseX - width / 2;
+    let my = mouseY - height / 2;
+    let pmx = pmouseX - width / 2;
+    let pmy = pmouseY - height / 2;
+    
+    if (mouseIsPressed) {
+      for (let i = 0; i < symmetry; i++) {
+        rotate(angle);
+        let sw = sizeSlider.value();
+        strokeWeight(vol*50);
+      
+        var sad = [0,70,70,110,140,180]
+        var angry = [180,255,20,150,10,90]
+        var pride = [20,255,0,255,20,255]
+        var happy = [80,255,0,100,50,200]
+        if (freq<300){
+          color = happy
+        }
+        else if (freq<500)
+          {
+            color = sad
+          }
+        else
+          {
+            color = angry
+          }
+        
+        
+        stroke(random(color[0],color[1]),random(color[2],color[3]),random(color[4],color[5]))
+        
+        line(mx, my, pmx, pmy);
+        push();
+        scale(1, -1);
+        line(mx, my, pmx, pmy);
+        pop();
+      }
+    }
+  }
+}
+function getFrequency(volume) {
+  // You may need to adjust the mapping based on your use case
+  let minFreq = 20; // Minimum frequency
+  let maxFreq = 20000; // Maximum frequency
+  
+  // Map the volume to a frequency range
+  let freq = map(volume, 0, 1, minFreq, maxFreq);
+  return freq;
+}
