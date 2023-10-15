54a2c4e5b42978cbff3e148f41fd55a2650c8aa2
Ramyani <ramyani.ghosh@gmail.com>
2023-10-14 22:23:00 -0400

    Update sketch.js
    Changed Color Palette
    Made Window width responsive
    Changed offset to make starting point different

--- sketch.js
+++ sketch.js
@@ -9,25 +9,28 @@
 // var palette2 = [[0,70,70,110,140,180],[50,80,100,190,100,140],[70,90,100,210,80,200],[140,255,100,230,90,120],[200,255,200,230,90,120]];
 
 let palettes = [[[0,70,70,110,140,180],[0,70,140,170,140,200],[80,255,0,100,50,200],[80,255,0,100,50,200],[200,255,80,120,180,220]],
-              [[50,80,100,190,100,140],[0,70,70,110,140,180],[70,90,100,210,80,200],[140,255,100,255,90,120],[240,255,200,230,120,160]],
+                [[30,50,60,110,100,110],[0,70,70,110,140,180],[70,90,100,210,80,200],[140,255,100,255,90,120],[240,255,200,230,120,160]],
                 [[30,80,40,80,120,180],[40,100,40,100,100,170],[70,90,70,110,150,190],[90,130,100,180,160,240],[180,210,160,200,230,255]],
                 [[80,130,50,100,50,130],[100,150,50,90,50,80],[170,210,90,120,80,90],[170,220,90,110,90,120],[240,255,200,220,150,190]]
 ];
 var palette;
 var lowfreq = [115,130,160,200]
-var highfreq = [170,200,290,400]
-let symmetry = 5;  
+var highfreq = [200,280,360,440]
+let symmetry = 10;  
 let angle = 360 / symmetry;
 let saveButton, clearButton, mouseButton, keyboardButton;
 let slider;
+var mywidth;
+var myheight;
 
 let freqmode = highfreq;
 
 
 function setup() { 
-  createCanvas(1010, 710);
+  createCanvas(windowWidth, windowHeight);
+  mywidth = int(windowWidth/2);
+  myheight = int(windowHeight/2);
   angleMode(DEGREES);
-  bg = loadImage('backgroundsky.jpeg');
   background(0);
 
   // Creating the save button for the file
@@ -74,20 +77,22 @@ function screenFull() {
 
 
 offset = 0;
-var rx = 525 + offset;
-var ry= 355 + offset;
+var rx = 550 + offset;
+var ry= 350  + offset;
 var  prx = rx;
 var  pry = ry;
 var t = 0;
 
 
 function draw() {
+  console.log(myheight);
+  console.log(mywidth);
   if (frameCount == 60*30) {
     noLoop();
     saveFile();
     console.log("Your aural visual is ready :)");
   }
-  
+
   
   let vol = mic.getLevel();
   let freq = getFrequency(vol);
@@ -95,9 +100,8 @@ function draw() {
   
   translate(width / 2, height / 2);
 
-  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height)   {
-    
-  
+  if (freq>20)   { 
+    console.log(width);
     let mx =  rx - width / 2;
     let my =  ry - height / 2;
     let pmx =  prx - width / 2;
@@ -105,54 +109,43 @@ function draw() {
     prx = rx;
     pry = ry;
     
-    if(mx > 300)
+    if(my > myheight+400)
       {
-          symmetry = random(5,7);
+          symmetry = random(4,7);
           angle = 360 / symmetry;
           offset = offset - 5;
-          if(offset<-300)
+          if(offset<- (mywidth-20))
           {
               offset = 0;
           }
-          rx = 505;
-          ry= 355;
+          rx = mywidth-10;
+          ry= myheight;
           prx = rx;
           pry = ry;
       }
-    if(int(t)%200==0)
+    if(int(t)%150==0)
     {
-        rx = prx+random(0,1);
-        ry= pry+random(0,1);
+        rx= prx+random(0,4);
+        ry= pry+random(0,4);
     }
     else{
-        rx = prx+random(-6,7);
-        ry= pry+random(-6,7);
+        rx = prx+random(-7,8);
+        ry= pry+random(-8,11);
     }
     
-
-      
       for (let i = 0; i < symmetry; i++) {
         rotate(angle);
         let sw = sizeSlider.value();
-        mystroke = vol*55+0.2
+        mystroke = vol*100+0.5;
         if(mystroke>2)
           {
             mystroke = 2;
           }
         strokeWeight(mystroke);
-        
-      
-        // var sad = [0,70,70,110,140,180]
-        // var angry = [180,255,20,150,10,90]
-        // var pride = [20,255,0,255,20,255]
-        // var happy = [80,255,0,100,50,200]
-        
+     
         var myalpha = 255
         //freq= freq-50;
-        
-        // var palette = random(palette1,palette2);
-        
-        
+
         if (freq<freqmode[0]){
             
             mycolor = palette[0]
@@ -179,7 +172,7 @@ function draw() {
             mycolor = palette[4]
             myalpha = 255
         }
-                                stroke(random(mycolor[0],mycolor[1]),random(mycolor[2],mycolor[3]),random(mycolor[4],mycolor[5]),myalpha)
+        stroke(random(mycolor[0],mycolor[1]),random(mycolor[2],mycolor[3]),random(mycolor[4],mycolor[5]),myalpha)
 
         
         line(mx, my, pmx, pmy);
