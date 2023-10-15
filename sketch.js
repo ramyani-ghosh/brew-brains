3dc855a86c6e14dfdb625b63418d2bb28556ee08
Ramyani <ramyani.ghosh@gmail.com>
2023-10-14 23:28:06 -0400

    Update sketch.js
    Cleaned up code, 
    Changed color palette,
    Removed buttons

--- sketch.js
+++ sketch.js
@@ -1,30 +1,23 @@
-/*
- * @name Kaleidoscope
- * @arialabel User draws thick black lines on the grey background and it is mirrored 5 times in a circle like a kaleidoscope
- * @description A kaleidoscope is an optical instrument with two or more reflecting surfaces tilted to each other in an angle. This example tries to replicate the behavior of a kaleidoscope. Set the number of reflections at the symmetry variable and start drawing on the screen. Adjust the brush size with the help of the slider. The clear screen as it says clears the screen. The save button will download a .jpg file of the art that you have created.
- */
-// Symmetry corresponding to the number of reflections. Change the number for different number of reflections 
 
-// var palette1= [[0,70,70,110,140,180],[0,70,140,170,140,200],[80,255,0,100,50,200],[80,255,0,100,50,200],[120,255,80,120,120,220]];
-// var palette2 = [[0,70,70,110,140,180],[50,80,100,190,100,140],[70,90,100,210,80,200],[140,255,100,230,90,120],[200,255,200,230,90,120]];
-
-let palettes = [[[0,70,70,110,140,180],[0,70,140,170,140,200],[80,255,0,100,50,200],[80,255,0,100,50,200],[200,255,80,120,180,220]],
-                [[30,50,60,110,100,110],[0,70,70,110,140,180],[70,90,100,210,80,200],[140,255,100,255,90,120],[240,255,200,230,120,160]],
-                [[30,80,40,80,120,180],[40,100,40,100,100,170],[70,90,70,110,150,190],[90,130,100,180,160,240],[180,210,160,200,230,255]],
+//All color palettes: Tangled, Summer, Ice, Diwali
+let palettes = [[[70,80,30,70,120,150],[40,70,70,120,120,180],[80,255,0,100,50,200],[80,255,0,100,50,200],[200,255,80,120,180,220]],
+                [[30,50,90,110,70,90],[0,70,110,180,100,150],[70,90,100,210,80,200],[140,205,180,255,90,120],[240,255,200,230,170,190]],
+                [[30,70,60,70,110,170],[50,70,40,90,140,160],[70,90,70,110,150,190],[90,130,100,180,160,240],[180,210,200,220,240,255]],
                 [[80,130,50,100,50,130],[100,150,50,90,50,80],[170,210,90,120,80,90],[170,220,90,110,90,120],[240,255,200,220,150,190]]
 ];
+
 var palette;
 var lowfreq = [115,130,160,200]
 var highfreq = [200,280,360,440]
 let symmetry = 10;  
 let angle = 360 / symmetry;
-let saveButton, clearButton, mouseButton, keyboardButton;
 let slider;
 var mywidth;
 var myheight;
+var divider = 100;
 
-let freqmode = highfreq;
-
+//Choose frequency mode depending on level of noise
+let freqmode = lowfreq;
 
 function setup() { 
   createCanvas(windowWidth, windowHeight);
@@ -32,29 +25,14 @@ function setup() {
   myheight = int(windowHeight/2);
   angleMode(DEGREES);
   background(0);
-
-  // Creating the save button for the file
-  saveButton = createButton('save');
-  saveButton.mousePressed(saveFile);
-
-  // Creating the clear screen button
-  clearButton = createButton('clear');
-  clearButton.mousePressed(clearScreen);
-
-  // Creating the button for Full Screen
-  fullscreenButton = createButton('Full Screen');
-  fullscreenButton.mousePressed(screenFull);
-
-  // Setting up the slider for the thickness of the brush
-  brushSizeSlider = createButton('Brush Size Slider');
-  sizeSlider = createSlider(1, 100, 4, 0.1);
     
   // Create an Audio input
   mic = new p5.AudioIn();
 
-  // start the Audio Input.
-  // By default, it does not .connect() (to the computer speakers)
+  // start the Audio Input
   mic.start();
+
+  //Color palette choice is random
   palette = palettes[int(random(4))];
 }
 
@@ -64,40 +42,28 @@ function saveFile() {
   save(file_name);
 }
 
-// Clear Screen function
-function clearScreen() {
-  background(0);
-}
-
-// Full Screen Function
-function screenFull() {
-  let fs = fullscreen();
-  fullscreen(!fs);
-}
-
-
+//Initial starting positions
 offset = 0;
 var rx = 550 + offset;
 var ry= 350  + offset;
-var  prx = rx;
-var  pry = ry;
+var prx = rx;
+var pry = ry;
 var t = 0;
 
-
+//Function called to draw every frame
 function draw() {
-  console.log(myheight);
-  console.log(mywidth);
+
+  //Stop after 30 seconds of frames and save image
   if (frameCount == 60*30) {
     noLoop();
     saveFile();
     console.log("Your aural visual is ready :)");
   }
 
-  
   let vol = mic.getLevel();
   let freq = getFrequency(vol);
   
-  
+  //translate image to scale to the canvas
   translate(width / 2, height / 2);
 
   if (freq>20)   { 
@@ -109,8 +75,10 @@ function draw() {
     prx = rx;
     pry = ry;
     
-    if(my > myheight+400)
-      {
+    if(my > myheight+400) {
+
+          //Reset center position, new symmetry value, and updated radial jump forward value.
+          divider = int(random(1,4))*100;
           symmetry = random(4,7);
           angle = 360 / symmetry;
           offset = offset - 5;
@@ -122,76 +90,73 @@ function draw() {
           ry= myheight;
           prx = rx;
           pry = ry;
-      }
-    if(int(t)%150==0)
-    {
-        rx= prx+random(0,4);
-        ry= pry+random(0,4);
+    }
+    if(int(t)%divider==0){
+        //jump ahead radially to ensure design progresses outwards from center
+        rx= prx+random(0,1);
+        ry= pry+random(0,1);
     }
     else{
         rx = prx+random(-7,8);
         ry= pry+random(-8,11);
     }
     
-      for (let i = 0; i < symmetry; i++) {
-        rotate(angle);
-        let sw = sizeSlider.value();
-        mystroke = vol*100+0.5;
-        if(mystroke>2)
-          {
-            mystroke = 2;
-          }
-        strokeWeight(mystroke);
-     
-        var myalpha = 255
-        //freq= freq-50;
+    for (let i = 0; i < symmetry; i++) {
 
-        if (freq<freqmode[0]){
-            
-            mycolor = palette[0]
-            // console.log(mycolor)
-            myalpha = 180
-        }
-        else if (freq<freqmode[1])
-        {
-            mycolor = palette[1]
-            myalpha = 200
-        }
-        else if (freq<freqmode[2])
-        {
-            mycolor = palette[2]
-            myalpha = 230
-        }
-        else if (freq<freqmode[3])
-        {
-            mycolor = palette[3]
-            myalpha = 255
-        }
-        else 
-        {
-            mycolor = palette[4]
-            myalpha = 255
-        }
-        stroke(random(mycolor[0],mycolor[1]),random(mycolor[2],mycolor[3]),random(mycolor[4],mycolor[5]),myalpha)
-
-        
-        line(mx, my, pmx, pmy);
-        // line(x,y,px,py);
-        push();
-        scale(1, -1);
-        line(mx, my, pmx, pmy);
-        // line(x,y,px,py);
-        pop();
-        t+=0.1;
+      rotate(angle);
+      //Vary stroke width based on audio volume
+      var mystroke = vol*100+0.5;
+      if(mystroke>2){
+          mystroke = 2;
       }
-    
+      strokeWeight(mystroke);
+
+      //Vary color and transparency based on frequency
+      var myalpha = 255;
+      var mycolor;
+
+      if (freq<freqmode[0]){
+          mycolor = palette[0]
+          myalpha = 180
+      }
+      else if (freq<freqmode[1])
+      {
+          mycolor = palette[1]
+          myalpha = 200
+      }
+      else if (freq<freqmode[2])
+      {
+          mycolor = palette[2]
+          myalpha = 230
+      }
+      else if (freq<freqmode[3])
+      {
+          mycolor = palette[3]
+          myalpha = 255
+      }
+      else 
+      {
+          mycolor = palette[4]
+          myalpha = 255
+      }
+      stroke(random(mycolor[0],mycolor[1]),random(mycolor[2],mycolor[3]),random(mycolor[4],mycolor[5]),myalpha)
+
+      //Draw line between previous and current point
+      line(mx, my, pmx, pmy);
+      push();
+      //Scale and draw reflection
+      scale(1, -1);
+      line(mx, my, pmx, pmy);
+      pop();
+
+      //Increment time(or frame)
+      t+=0.1;
+    }
   }
 }
 function getFrequency(volume) {
-  // You may need to adjust the mapping based on your use case
   let minFreq = 20; // Minimum frequency
   let maxFreq = 20000; // Maximum frequency
-  
   // Map the volume to a frequency range
   let freq = map(volume, 0, 1, minFreq, maxFreq);
   return freq;
